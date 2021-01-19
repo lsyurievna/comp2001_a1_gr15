07d6c86edec5cf9f2a21fae7a3560e4ae9feb828
Liudmila Strelnikova <lsyurievna@mun.ca>
2021-01-19 14:09:16 +0300

    Initial sharing of project

---
+++ Bicycle.java
@@ -0,0 +1,29 @@
+/**
+ * A class that maintains information on a bicycle.
+ * This might form part of a larger application such
+ * as a bicycle shop, for instance.
+ *
+ * @author (Insert name of group member 1)
+ * @author (Insert name of group member 2)
+ * @author (Insert name of group member 3)
+ * 
+ * @version (Insert today's date here.)
+ */
+public class Bicycle
+{
+    // The fields.
+    private String owner;
+    private String model;
+
+    /**
+     * Set the owner and model fields when this object
+     * is constructed.
+     */
+    public Bicycle(String bikeOwner, String bikeModel)
+    {
+        owner = bikeOwner;
+        model = bikeModel;
+    }
+
+    // Add the methods here ...
+}
