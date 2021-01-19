465e741a7832c47aedafec8846ca49774c89daab
Liudmila Strelnikova <lsyurievna@mun.ca>
2021-01-19 16:06:02 +0300

    First three tasks from the assignment done. 1. Add accessor methods for owner and model. 2. Add fields sprocketNumber and chainringNumber that will keep info about number of sprockets and chainrings. Add accessor methods for these two fields. 3. Add two methods that set the number of sprockets and chainrings. Mila.

--- Bicycle.java
+++ Bicycle.java
@@ -3,17 +3,19 @@
  * This might form part of a larger application such
  * as a bicycle shop, for instance.
  *
- * @author (Insert name of group member 1)
+ * @author Liudmila Strelnikova
  * @author (Insert name of group member 2)
  * @author (Insert name of group member 3)
  * 
- * @version (Insert today's date here.)
+ * @version 19.01.2021
  */
 public class Bicycle
 {
     // The fields.
     private String owner;
     private String model;
+    private int sprocketNumber;
+    private int chainringNumber;
 
     /**
      * Set the owner and model fields when this object
@@ -23,7 +25,75 @@ public class Bicycle
     {
         owner = bikeOwner;
         model = bikeModel;
+        sprocketNumber = 0;
+        chainringNumber = 0;
     }
 
-    // Add the methods here ...
+    /**
+     * Gets the bike's model.
+     * @return String model the a bike 
+     */
+    public String getModel()
+    {
+        return model;
+    }
+    
+    /**
+     * Gets the bike's owner.
+     * @return String owner of the bike 
+     */
+    public String getOwner()
+    {
+        return owner;
+    }
+    
+    /**
+     * Gets the number of sprockets in the bike.
+     * @return int number of sprockets in the bike 
+     */
+    public int getSprocketNumber()
+    {
+        return sprocketNumber;
+    }
+    
+    /**
+     * Gets the number of chainrings in the bike.
+     * @return int number of chainrings in the bike 
+     */
+    public int getChainringNumber()
+    {
+        return chainringNumber;
+    }
+    
+    /**
+     * Sets the number of chainrings for the bike.
+     * @param bikeChainringNumber number of chainrings in the bike 
+     */
+    public void setChainringNumber(int bikeChainringNumber)
+    {   if (bikeChainringNumber >= 0) //I allow the number to be 0 in case it is a broken bike
+        {
+            chainringNumber = bikeChainringNumber;
+        }
+        else
+        {
+            System.out.println("Enter a positive integer.");
+        }
+    }
+    
+    /**
+     * Sets the number of chainrings for the bike.
+     * @param bikeSprocketNumber number of chainrings in the bike 
+     */
+    public void setSprocketNumber(int bikeSprocketNumber)
+    {
+        if (bikeSprocketNumber >= 0) //I allow the number to be 0 in case it is a broken bike
+        {
+            sprocketNumber = bikeSprocketNumber;
+        }
+        else
+        {
+            System.out.println("Enter a positive integer.");
+        }
+        
+    }
 }
